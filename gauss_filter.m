9529babc387115a2cab972c80b5ce9dea2df1d15
Andrew Barentine <Andrew@Andrews-MacBook-Pro-5.local>
2017-10-11 10:21:11 -0400

    adding gauss filter

---
+++ gauss_filter.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = gauss_filter(inputArg1,inputArg2)
+%GAUSS_FILTER Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
