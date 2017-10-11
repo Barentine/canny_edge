36f5e82a22938e5ddb12f15e49dc98eb76070766
Andrew Barentine <Andrew@Andrews-MacBook-Pro-5.local>
2017-10-11 14:36:44 -0400

    finished nonmaximal_sup

--- gradient.m
+++ gradient.m
@@ -1,4 +1,4 @@
-function [grad] = gradient(image)
+function [grad, angles] = gradient(image)
 %GRADIENT calculates the gradient using forward derivatives - beware edge
 % effects
 im_size = size(image);
@@ -6,14 +6,15 @@ im_size = size(image);
 % first take column-wise derivative
 right_shifted = zeros(im_size);
 right_shifted(:, 2:end) = image(:, 1:end - 1);
-x_der = -(right_shifted - image);  % negate because I wanted forward deriv.
+c_der = -(right_shifted - image);  % negate because I wanted forward deriv.
 
 % now take the row-wise derivative
 down_shifted = zeros(im_size);  % note that for speed, we could reuse right_shifted if we wanted to
 down_shifted(2:end, :) = image(1:end-1, :);
-y_der = -(down_shifted - image);
+r_der = -(down_shifted - image);
 
 % determine the overall gradient
-grad = sqrt(x_der.^2 + y_der.^2);
+grad = sqrt(r_der.^2 + c_der.^2);
+angles = atan2(r_der, c_der);
 end
 
--- nonmaximal_sup.m
+++ nonmaximal_sup.m
@@ -1,7 +1,55 @@
-function [maximal_grad] = nonmaximal_sup(gradient)
+function [maximal_grad] = nonmaximal_sup(grad, angles)
 %NONMAXIMAL_SUP Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+%   angles should be from matlab's atan2, i.e. range -pi,pi
+
+% generate coordinates
+im_size = size(grad);
+[cc, rr] = meshgrid(1:im_size(1), 1:im_size(2));
+
+% calculate radius-of-one displacements parallel to gradient
+r_max = rr + sin(angles);
+c_max = cc + cos(angles);
+% calculate gradient 1-pixel away along gradient
+par_g = interp2(grad,r_max,c_max);
+
+% calculate radius-of-one displacements ANTI-parallel to gradient
+r_max = rr - sin(angles);
+c_max = cc - cos(angles);
+% calculate gradient 1-pixel away along gradient
+anti_par_g = interp2(grad,r_max,c_max);
+
+nms = grad >= par_g & grad >= anti_par_g;
+maximal_grad = nms.*grad;
+
+%% --- ONLY SAD MISTAKES BELOW THIS LINE
+% %[cc, rr] = meshgrid(size)
+% 
+% 
+% % pad the gradients with zeros
+% interp_extent = 2;
+% pad_size = interp_extent;
+% g_pad = zeros(im_size + 2*pad_size);
+% g_pad(1+pad_size:pad_size+im_size(1), 1+pad_size:pad_size+im_size(2)) = grad;
+
+% loop through image, calculating at each point whether it is a local
+% maximum
+% [cc, rr] = meshgrid(-interp_extent:interp_extent, -interp_extent:interp_extent);
+% cc = cc+3;
+% rr = rr+3;
+% for pr=pad_size:pad_size + im_size(1)
+%     for pc=pad_size:pad_size + im_size(2)     
+%         % ----- interpolate around each pixel
+%         % crop to smaller size
+%         crop_g = grad(1+pr-interp_extent:1+pr+interp_extent, 1+pc-interp_extent:1+pc+interp_extent);
+%         
+%         % interpolate and calculate magnitude
+%         interp_g = interp2(rr,cc,crop_g,r_max,c_max);
+%         
+%         
+%         
+%         
+%     end
+% end
+% 
+% end
 
