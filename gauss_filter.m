f38c94d433f3f6746291758dee9c3ec8ce9ef398
Hanh Lam <hanhlam@Hanhs-MacBook-Pro.local>
2017-10-11 11:33:42 -0400

    “filtersimple”

--- gauss_filter.m
+++ gauss_filter.m
@@ -1,7 +1,14 @@
-function [outputArg1,outputArg2] = gauss_filter(inputArg1,inputArg2)
-%GAUSS_FILTER Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+function [outputArg1] = gauss_filter(image,size)
+%GAUSS_FILTER takes preloaded <image> input and apply gaussian filter
+%ising Matlab Gauss filter fspecial and smooth function imfilter, with 
+%   hsize <size> input
 
+% gaussian filter
+gauss = fspecial('gaussian',size,5);
+% smoothing
+outputArg1 = imfilter(image,gauss);
+
+end
+% to call
+% image = double(imread('data/xy001-1.png'));
+% imagesc(gauss_filter(image,10))
